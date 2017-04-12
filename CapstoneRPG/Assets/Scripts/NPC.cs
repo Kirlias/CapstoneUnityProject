ef7576a6acbaed7655538e9271872cf51943cc83
Kirlias <alduinworldeater@gmail.com>
2017-04-12 11:53:13 -0300

    Player will move

---
+++ CapstoneRPG/Assets/Scripts/NPC.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MovingObject {
+
+
+    protected override void OnCantMove<T>(T component)
+    {
+        
+    }
+
+    // Use this for initialization
+    protected override void Start () {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit2D hit;
+        Move(2, 2, out hit);
+
+	}
+}
