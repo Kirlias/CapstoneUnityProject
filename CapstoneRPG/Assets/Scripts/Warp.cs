9b585f39c4279bd854fe01a2c9879c8bbc71e4ef
w0285037 <w0285037@nscc.ca>
2017-04-13 12:43:00 -0300

    Cave made. Warp and screen fade working

---
+++ CapstoneRPG/Assets/Scripts/Warp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour {
+
+    public Transform warpTarget;
+
+    IEnumerator OnTriggerEnter2D(Collider2D Other) {
+
+        Debug.Log("Object collided");
+        if (true)
+        {
+            ScreenFader sf = GameObject.FindGameObjectWithTag("Fader").GetComponent<ScreenFader>();
+
+            yield return StartCoroutine(sf.FadeToBlack());
+
+            Other.gameObject.transform.position = warpTarget.position;
+
+            yield return StartCoroutine(sf.FadeToClear());
+        }
+    }
+
+}
